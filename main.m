edb8ede39a86a415a7bce42c523f6fa890d891b4
andy <andyshenproper@gmail.com>
2021-07-26 14:03:51 -0700

    Read images from subfolder. Update readme. Stop processing of images when image with same filename already exists

--- main.m
+++ main.m
@@ -1,22 +1,32 @@
-folder = 'images';
+folder = 'images\**';
+export_folder = 'exports\';
 images = dir(fullfile(folder, '\*.jpg'));
 
+length = numel(images);
 
-for i = 1:numel(images)
-    filename = fullfile(folder, images(i).name);
-    evaluateCC(filename);
+for i = 1:length
+    imgname = images(i).name;
+    foldername = images(i).folder;
+    filename = fullfile(foldername, imgname);
+    
+    if ~isfile(fullfile(export_folder, 'greyworld', imgname)) && ~isfile(fullfile(export_folder, 'shadesofgrey', imgname))
+        fprintf('Processing %d of %d: %s...\n', i, length, filename);
+        evaluateCC(filename, imgname);
+    end
 end
     
-function evaluateCC(filename)
+function evaluateCC(filename, imgname)
+    export_folder = 'exports\';
+
     input_im = double(imread(filename));
     
     [wR,wG,wB,out1]=shades_of_grey(input_im,1);
     greyWorld = uint8(out1);
-    imwrite(greyWorld,strcat("export-greyworld-", filename));
+    imwrite(greyWorld, fullfile(export_folder, 'greyworld', imgname));
     
     p=6;
     [wR,wG,wB,out2]=shades_of_grey(input_im,p);
     shadesOfGrey = uint8(out2);
-    imwrite(shadesOfGrey,strcat("export-shadesofgrey-", filename));
+    imwrite(shadesOfGrey, fullfile(export_folder, 'shadesofgrey', imgname));
         
 end
